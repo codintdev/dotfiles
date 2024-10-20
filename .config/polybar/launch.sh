f6c0aff08e19af2438e3d3e444716019a2d2f0fb
codintdev <codintdev@gmail.com>
2024-10-20 12:40:26 -0500

    Latest Version | Update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -18,10 +18,10 @@ polybar cuaternary -c ~/.config/polybar/current.ini &
 ## Right bar
 #polybar top -c ~/.config/polybar/current.ini &
 polybar primary -c ~/.config/polybar/current.ini &
-polybar quinary -c ~/.config/polybar/current.ini &
+#polybar quinary -c ~/.config/polybar/current.ini &
 #polybar secondary -c ~/.config/polybar/current.ini &
-#polybar sexto -c ~/.config/polybar/current.ini &
-#polybar septimo -c ~/.config/polybar/current.ini &
+polybar sexto -c ~/.config/polybar/current.ini &
+polybar septimo -c ~/.config/polybar/current.ini &
 
 ## Center bar
 polybar primary -c ~/.config/polybar/workspace.ini &
